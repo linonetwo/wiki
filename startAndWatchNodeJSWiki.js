0ec4d2cd799486687823b5ecab55092d9c708564
linonetwo <linonetwo012@gmail.com>
2020-04-10 11:58:17 +0800

    faet: ignore __StoryList change

--- startAndWatchNodeJSWiki.js
+++ startAndWatchNodeJSWiki.js
@@ -4,12 +4,17 @@ const $tw = require('tiddlywiki/boot/boot.js').TiddlyWiki();
 const execSync = require('child_process').execSync;
 
 const tiddlyWikiPort = require('./package.json').port;
-const wikiFolderName = require('./package.json').name
+const wikiFolderName = require('./package.json').name;
 
 const projectFolder = path.dirname(__filename);
 const tiddlyWikiFolder = path.join(projectFolder, wikiFolderName);
 const commitScriptPath = path.resolve(projectFolder, 'scripts', 'commit.sh');
 const buildHTMLScriptPath = path.resolve(projectFolder, 'scripts', 'buildHTML.sh');
+const frequentlyChangedFileThatShouldBeIgnoredFromWatch = [
+  'output',
+  'tiddlers/$__StoryList.tid',
+  'tiddlers/$__plugins_felixhayashi_tiddlymap_misc_defaultViewHolder.tid',
+];
 
 $tw.boot.argv = [tiddlyWikiFolder, '--listen', `port=${tiddlyWikiPort}`];
 
@@ -45,7 +50,9 @@ fs.watch(
   tiddlyWikiFolder,
   { recursive: true },
   debounce((_, fileName) => {
-    if (['output'].includes(fileName)) return;
+    if (frequentlyChangedFileThatShouldBeIgnoredFromWatch.includes(fileName)) {
+      return;
+    }
     console.log(`${fileName} change`);
 
     buildHTMLEveryMinute();
