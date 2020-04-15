12d2b4f70bd3aaff0590c52ecf13aed9210325ca
linonetwo <linonetwo012@gmail.com>
2020-04-16 01:38:13 +0800

    fix

--- getTiddlersJSON.js
+++ getTiddlersJSON.js
@@ -8,7 +8,9 @@ const repoFolder = path.dirname(__filename);
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
 module.exports = () =>
-  fetch(`http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json`)
+  fetch(
+    `http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json?filter=%5Ball%5Btiddlers%5D%5D%20-%5B%5B%24%3A%2FisEncrypted%5D%5D%20-%5Bprefix%5B%24%3A%2Ftemp%2F%5D%5D%20-%5Bprefix%5B%24%3A%2Fstatus%2F%5D%5D`
+  )
     .then((response) => response.text())
     .then((tiddlersJSON) => {
       if (tiddlersJSON) {
