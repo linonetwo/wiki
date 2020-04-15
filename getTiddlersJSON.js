2bb52cd407269cdd64dfec5670419da9a437325c
linonetwo <linonetwo012@gmail.com>
2020-04-16 01:28:24 +0800

    Update getTiddlersJSON.js

--- getTiddlersJSON.js
+++ getTiddlersJSON.js
@@ -9,7 +9,7 @@ const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
 module.exports = () =>
   fetch(
-    `http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json?filter=[all[tiddlers]] -[[%24%3A%2FisEncrypted]] -[prefix[%24%3A%2Ftemp%2F]] -[prefix[%24%3A%2Fstatus%2F]]`
+    `http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json]`
   )
     .then((response) => response.text())
     .then((tiddlersJSON) => fs.writeFileSync(path.join(tiddlyWikiFolder, 'public', 'tiddlers.json'), tiddlersJSON));
