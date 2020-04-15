edc70f9a3d69060de551ea9b000cab023d3779af
linonetwo <linonetwo012@gmail.com>
2020-04-16 01:34:18 +0800

    Update getTiddlersJSON.js

--- getTiddlersJSON.js
+++ getTiddlersJSON.js
@@ -8,12 +8,12 @@ const repoFolder = path.dirname(__filename);
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
 module.exports = () =>
-  fetch(`http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json]`)
+  fetch(`http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json`)
     .then((response) => response.text())
     .then((tiddlersJSON) => {
       if (tiddlersJSON) {
         fs.writeFileSync(path.join(tiddlyWikiFolder, 'public', 'tiddlers.json'), tiddlersJSON);
       } else {
-        throw new Error('getting tiddlers.json failed! Please keek your server up before commit.');
+        throw new Error('getting tiddlers.json failed! Please keep your server up before commit.');
       }
     });
