663302c1965f8b054f9c509ced15f9d9c0869018
linonetwo <linonetwo012@gmail.com>
2020-04-16 01:41:33 +0800

    fix

--- getTiddlersJSON.js
+++ getTiddlersJSON.js
@@ -9,13 +9,15 @@ const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
 module.exports = () =>
   fetch(
-    `http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json?filter=%5Ball%5Btiddlers%5D%5D%20-%5B%5B%24%3A%2FisEncrypted%5D%5D%20-%5Bprefix%5B%24%3A%2Ftemp%2F%5D%5D%20-%5Bprefix%5B%24%3A%2Fstatus%2F%5D%5D`
+    `http://127.0.0.1:${tiddlyWikiPort}/recipes/default/tiddlers.json?filter=[all[tiddlers]] -[[%24%3A%2FisEncrypted]] -[prefix[%24%3A%2Ftemp%2F]] -[prefix[%24%3A%2Fstatus%2F]]`
   )
     .then((response) => response.text())
     .then((tiddlersJSON) => {
       if (tiddlersJSON) {
         fs.writeFileSync(path.join(tiddlyWikiFolder, 'public', 'tiddlers.json'), tiddlersJSON);
       } else {
+        console.log(tiddlersJSON);
+        
         throw new Error('getting tiddlers.json failed! Please keep your server up before commit.');
       }
     });
