cd98d9a898e6fdf1ecfbb25614cb39cf79669d5f
linonetwo <linonetwo012@gmail.com>
2020-04-30 00:42:00 +0800

    feat: 把GoogleCalendar导入到TiddlyWiki上

--- MemeOfLinonetwo/tiddlers/导入GoogleCalendar的按钮.js
+++ MemeOfLinonetwo/tiddlers/导入GoogleCalendar的按钮.js
@@ -4,6 +4,7 @@ module-type: widget
 
 Import Google Calendar event of today into TiddlyWiki
 Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
+Attributes: yesterday="yes"
 
 \*/
 (function () {
@@ -14,6 +15,9 @@ Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
   const Widget = require('$:/core/modules/widgets/widget.js').widget;
 
   class GoogleCalendarToTiddlyWikiWidget extends Widget {
+    /**
+     * Lifecycle method: call this.initialise and super
+     */
     constructor(parseTreeNode, options) {
       super(parseTreeNode, options);
       this.initialise(parseTreeNode, options);
@@ -22,9 +26,9 @@ Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
       };
     }
 
-    /*
-    Render this widget into the DOM
-    */
+    /**
+     * Lifecycle method: Render this widget into the DOM
+     */
     render(parent, nextSibling) {
       this.parentDomNode = parent;
       this.computeAttributes();
@@ -35,12 +39,16 @@ Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
       importButton.onclick = this.onImportButtonClick.bind(this);
       parent.insertBefore(importButton, nextSibling);
       this.domNodes.push(importButton);
+      this.initClient();
     }
 
+    /**
+     * Event listener of button
+     */
     async onImportButtonClick() {
       if (!this.state.isSignedIn) {
         try {
-          await this.initClient();
+          // await this.initClient(); // we have init it in the constructor
           gapi.auth2.getAuthInstance().signIn();
         } catch (error) {
           console.error('GoogleCalendarToTiddlyWikiWidget: Error login using gapi client', error);
@@ -59,6 +67,13 @@ Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
      *  listeners.
      */
     async initClient() {
+      console.log('init');
+      // on start up, it might not be loaded, we schedule it later
+      if (!window.gapi) {
+        setTimeout(this.initClient.bind(this), 100);
+        return;
+      }
+
       // Client ID and API key from the Google Developer Console
       // we get it from tiddler
       const CLIENT_ID = $tw.wiki.getTiddler('GoogleCalendarCLIENT_ID').fields.text;
@@ -87,46 +102,119 @@ Usage: <$import-google-calendar-event tags="private GoogleCalendar" />
      *  appropriately. After a sign-in, the API is called.
      */
     updateSignInStatus(isSignedIn) {
+      const prevState = { ...this.state };
       if (isSignedIn) {
         this.state.isSignedIn = true;
       } else {
         this.state.isSignedIn = false;
       }
       // this is like React forceUpdate, we use it because it is not fully reactive on this.state change
-      this.refreshSelf();
+      if (prevState.isSignedIn !== isSignedIn) {
+        this.refreshSelf(); // method from super class
+      }
     }
 
+    /**
+     * Using $tw to add fetched calender data to TiddlerWiki, and add tags to them
+     */
     async importToWiki() {
-      console.log(await this.getTodayEvents());
+      const tags = this.getAttribute('tags', '');
+
+      const calendarList = await this.getCalendarLists();
+      const calendarEvents = await this.getCalendarEvents(calendarList);
+      const tiddlers = calendarEvents.map(
+        ({
+          id,
+          start: { dateTime: startDate },
+          end: { dateTime: endDate },
+          summary,
+          description = '',
+          created,
+          updated = created,
+          creator,
+          htmlLink,
+          organizer: { displayName: category },
+          color,
+        }) => ({
+          title: `GoogleCalendar/${id}`,
+          caption: summary,
+          text: description,
+          tags: `${tags} ${category}`,
+          type: 'text/vnd.tiddlywiki',
+          startDate,
+          endDate,
+          created: new Date(created).toTWUTCString(),
+          creator: creator.email || 'GoogleCalendar',
+          modified: new Date(updated).toTWUTCString(),
+          source: htmlLink,
+          color, // mixed from calendar data
+        })
+      );
+
+      $tw.wiki.addTiddlers(tiddlers);
+    }
+
+    /**
+     * Get calendars we want to import
+     */
+    async getCalendarLists() {
+      const calendarListResponse = await gapi.client.calendar.calendarList.list({ showDeleted: false });
+      const calendarList = calendarListResponse.result.items;
+      // I set every calendar need to be imported have a description starts with '任务类型'
+      return calendarList.filter((calendar) => String(calendar.description).startsWith('任务类型'));
     }
 
     /**
      * Get list of Calendar events, modify this if you want to customize it for your need
      */
-    async getTodayEvents() {
-      const calendarListResponse = await gapi.client.calendar.calendarList.list({ showDeleted: false });
-      const calendarList = calendarListResponse.result.items;
-      // I set every calendar need to be imported have a description starts with '任务类型'
+    async getCalendarEvents(calendarList) {
+      // set date range
+      let timeMin = new Date(new Date().setHours(0, 0, 0, 0)).toISOString();
+      let timeMax = new Date(new Date().setHours(24, 0, 0, 0)).toISOString();
+      const getYesterday = this.getAttribute('yesterday', 'no') === 'yes';
+      if (getYesterday) {
+        timeMin = new Date(new Date().setHours(-24, 0, 0, 0)).toISOString();
+        timeMax = new Date(new Date().setHours(0, 0, 0, 0)).toISOString();
+      }
+
       const allEventList = await Promise.all(
-        calendarList
-          .filter((calendar) => String(calendar.description).startsWith('任务类型'))
-          .map((calendar) => {
-            // get all events in the calendar
-            return gapi.client.calendar.events
-              .list({
-                calendarId: calendar.id,
-                // from midnight to next midnight
-                timeMin: new Date(new Date().setHours(0, 0, 0, 0)).toISOString(),
-                timeMax: new Date(new Date().setHours(24, 0, 0, 0)).toISOString(),
-              })
-              .then((eventResponse) => {
-                return eventResponse.result.items;
-              });
-          })
+        calendarList.map((calendar) => {
+          const calendarColor = calendar.backgroundColor;
+          // get all events in the calendar
+          return gapi.client.calendar.events
+            .list({
+              calendarId: calendar.id,
+              // from midnight to next midnight
+              timeMin,
+              timeMax,
+            })
+            .then((eventResponse) => {
+              return eventResponse.result.items.map((item) => ({ ...item, color: calendarColor }));
+            });
+        })
       );
       return allEventList.flat();
     }
   }
 
   exports['import-google-calendar-event'] = GoogleCalendarToTiddlyWikiWidget;
+
+  // Utils
+  function pad(number) {
+    if (number < 10) {
+      return '0' + number;
+    }
+    return number;
+  }
+  Date.prototype.toTWUTCString = function toTWUTCString() {
+    return (
+      this.getUTCFullYear() +
+      pad(this.getUTCMonth() + 1) +
+      pad(this.getUTCDate()) +
+      pad(this.getUTCHours()) +
+      pad(this.getUTCMinutes()) +
+      pad(this.getUTCSeconds()) +
+      (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5)
+    );
+  };
 })();
