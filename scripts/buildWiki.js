21e6ade366f89c769726bf8d479bc05e4a0e6511
linonetwo <linonetwo012@gmail.com>
2020-06-03 18:39:36 +0800

    chore: simplify build process

--- scripts/buildWiki.js
+++ scripts/buildWiki.js
@@ -7,36 +7,36 @@ const repoFolder = path.join(path.dirname(__filename), '..');
 
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
+// cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='MemeOfLinonetwo/themes'
 process.env['TIDDLYWIKI_PLUGIN_PATH'] = `${tiddlyWikiFolder}/plugins`;
 process.env['TIDDLYWIKI_THEME_PATH'] = `${tiddlyWikiFolder}/themes`;
 
+const execAndLog = (command, options) => console.log(String(execSync(command, options)));
+
 module.exports = function build() {
   // npm run build:prepare
-  execSync(`rm -rf ${repoFolder}/public`);
+  execAndLog(`rm -rf ${repoFolder}/public`);
   // npm run build:public
-  execSync(
+  execAndLog(
     `cp -r ./MemeOfLinonetwo/public/ ./public && cp ./MemeOfLinonetwo/tiddlers/favicon.ico ./public/favicon.ico && cp ./MemeOfLinonetwo/tiddlers/TiddlyWikiIconWhite.png ./public/TiddlyWikiIconWhite.png && cp ./MemeOfLinonetwo/tiddlers/TiddlyWikiIconBlack.png ./public/TiddlyWikiIconBlack.png`,
     { cwd: repoFolder }
   );
   // npm run build:nodejs2html
-  execSync(`tiddlywiki MemeOfLinonetwo --build externalimages`, { cwd: repoFolder });
+  execAndLog(`tiddlywiki MemeOfLinonetwo --build externalimages`, { cwd: repoFolder });
   // npm run build:sitemap
-  execSync(
+  execAndLog(
     `tiddlywiki MemeOfLinonetwo --rendertiddler sitemap sitemap.xml text/plain && mv ./MemeOfLinonetwo/output/sitemap.xml ./public/sitemap.xml`,
     { cwd: repoFolder }
   );
   // npm run build:minifyHTML
-  execSync(
+  execAndLog(
     `html-minifier-terser -c ./html-minifier-terser.config.json -o ./public/index.html ./MemeOfLinonetwo/output/index.html`,
     { cwd: repoFolder }
   );
   // npm run build:precache
-  execSync(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
+  execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
   // npm run build:clean
-  execSync(`rm -r ./MemeOfLinonetwo/output`, { cwd: repoFolder });
+  execAndLog(`rm -r ./MemeOfLinonetwo/output`, { cwd: repoFolder });
   // npm run build:pluginLibrary
-  execSync(
-    `rm -rf node_modules/tiddlywiki/plugins/published && mkdir -p node_modules/tiddlywiki/plugins/published && cp -r MemeOfLinonetwo/plugins node_modules/tiddlywiki/plugins/published && tiddlywiki MemeOfLinonetwo --output public/library --build library`,
-    { cwd: repoFolder }
-  );
+  execAndLog(`tiddlywiki MemeOfLinonetwo --output public/library --build library`, { cwd: repoFolder });
 };
