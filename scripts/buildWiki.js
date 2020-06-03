cf9d00d2871516e9f2729a8ead120742902ed6c4
linonetwo <linonetwo012@gmail.com>
2020-06-03 19:41:53 +0800

    Update buildWiki.js

--- scripts/buildWiki.js
+++ scripts/buildWiki.js
@@ -7,7 +7,7 @@ const repoFolder = path.join(path.dirname(__filename), '..');
 
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 
-// cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='MemeOfLinonetwo/themes'
+// cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='${wikiFolderName}/themes'
 process.env['TIDDLYWIKI_PLUGIN_PATH'] = `${tiddlyWikiFolder}/plugins`;
 process.env['TIDDLYWIKI_THEME_PATH'] = `${tiddlyWikiFolder}/themes`;
 
@@ -18,25 +18,25 @@ module.exports = function build() {
   execAndLog(`rm -rf ${repoFolder}/public`);
   // npm run build:public
   execAndLog(
-    `cp -r ./MemeOfLinonetwo/public/ ./public && cp ./MemeOfLinonetwo/tiddlers/favicon.ico ./public/favicon.ico && cp ./MemeOfLinonetwo/tiddlers/TiddlyWikiIconWhite.png ./public/TiddlyWikiIconWhite.png && cp ./MemeOfLinonetwo/tiddlers/TiddlyWikiIconBlack.png ./public/TiddlyWikiIconBlack.png`,
+    `cp -r ${tiddlyWikiFolder}/public/ ./public && cp ${tiddlyWikiFolder}/tiddlers/favicon.ico ./public/favicon.ico && cp ${tiddlyWikiFolder}/tiddlers/TiddlyWikiIconWhite.png ./public/TiddlyWikiIconWhite.png && cp ${tiddlyWikiFolder}/tiddlers/TiddlyWikiIconBlack.png ./public/TiddlyWikiIconBlack.png`,
     { cwd: repoFolder }
   );
   // npm run build:nodejs2html
-  execAndLog(`tiddlywiki MemeOfLinonetwo --build externalimages`, { cwd: repoFolder });
+  execAndLog(`tiddlywiki ${wikiFolderName} --build externalimages`, { cwd: repoFolder });
   // npm run build:sitemap
   execAndLog(
-    `tiddlywiki MemeOfLinonetwo --rendertiddler sitemap sitemap.xml text/plain && mv ./MemeOfLinonetwo/output/sitemap.xml ./public/sitemap.xml`,
+    `tiddlywiki ${wikiFolderName} --rendertiddler sitemap sitemap.xml text/plain && mv ${tiddlyWikiFolder}/output/sitemap.xml ./public/sitemap.xml`,
     { cwd: repoFolder }
   );
   // npm run build:minifyHTML
   execAndLog(
-    `html-minifier-terser -c ./html-minifier-terser.config.json -o ./public/index.html ./MemeOfLinonetwo/output/index.html`,
+    `html-minifier-terser -c ./html-minifier-terser.config.json -o ./public/index.html ${tiddlyWikiFolder}/output/index.html`,
     { cwd: repoFolder }
   );
   // npm run build:precache
   execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
   // npm run build:clean
-  execAndLog(`rm -r ./MemeOfLinonetwo/output`, { cwd: repoFolder });
+  execAndLog(`rm -r ${tiddlyWikiFolder}/output`, { cwd: repoFolder });
   // npm run build:pluginLibrary
-  execAndLog(`tiddlywiki MemeOfLinonetwo --output public/library --build library`, { cwd: repoFolder });
+  execAndLog(`tiddlywiki ${wikiFolderName} --output public/library --build library`, { cwd: repoFolder });
 };
