ba47a3b38154bc22d1033d20f520508791573915
Lin Onetwo <linonetwo012@gmail.com>
2020-07-06 13:46:07 +0800

    feat: recreate bob config using js

--- scripts/startNodeJSWiki.js
+++ scripts/startNodeJSWiki.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const execSync = require('child_process').execSync;
 const $tw = require('tiddlywiki/boot/boot.js').TiddlyWiki();
 
@@ -10,20 +11,22 @@ const repoFolder = path.join(path.dirname(__filename), '..');
 
 const tiddlyWikiFolder = path.join(repoFolder, wikiFolderName);
 const tiddlersFolder = path.join(tiddlyWikiFolder, 'tiddlers');
+const bobServerConfigPath = path.join(tiddlyWikiFolder, 'settings', 'settings.json');
+
+const bobServerConfig = JSON.parse(fs.readFileSync(bobServerConfigPath, 'utf-8'));
+bobServerConfig.pluginsPath = `./${wikiFolderName}/plugins`;
+bobServerConfig.themesPath = `./${wikiFolderName}/themes`;
+bobServerConfig.editionsPath = `./${wikiFolderName}/`;
+bobServerConfig.wikiPathBase = repoFolder;
+bobServerConfig['ws-server'].port = tiddlyWikiPort;
+fs.writeFileSync(bobServerConfigPath, JSON.stringify(bobServerConfig, null, '  '));
 
 process.env['TIDDLYWIKI_PLUGIN_PATH'] = `${tiddlyWikiFolder}/plugins`;
 process.env['TIDDLYWIKI_THEME_PATH'] = `${tiddlyWikiFolder}/themes`;
 // add tiddly filesystem back https://github.com/Jermolene/TiddlyWiki5/issues/4484#issuecomment-596779416
 $tw.boot.argv = [
-  '+plugins/tiddlywiki/filesystem',
-  '+plugins/tiddlywiki/tiddlyweb',
   '+plugins/OokTech/Bob',
   tiddlyWikiFolder,
-  '--listen',
-  `anon-username=${userName}`,
-  `port=${tiddlyWikiPort + 1}`,
-  'host=0.0.0.0',
-  'root-tiddler=$:/core/save/lazy-images',
   '--wsserver', // port config in Meme-of-LinOnetwo/settings/settings.json
 ];
 
