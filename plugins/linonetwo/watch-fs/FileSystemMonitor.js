50ebf31de86fb0415709dc7c3ad5e5d2a7137665
linonetwo <linonetwo012@gmail.com>
2020-08-22 20:48:29 +0800

    Wiki updated with TiddlyGit-Desktop

--- plugins/linonetwo/watch-fs/FileSystemMonitor.js
+++ plugins/linonetwo/watch-fs/FileSystemMonitor.js
@@ -15,7 +15,7 @@ const isNotNonTiddlerFiles = (filePath) =>
   !filePath.includes('.git');
 
 function FileSystemMonitor() {
-  const isDebug = false;
+  const isDebug = true;
   const debugLog = isDebug ? console.log : () => {};
 
   exports.name = 'watch-fs_FileSystemMonitor';
