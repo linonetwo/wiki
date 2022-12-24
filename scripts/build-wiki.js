6996d6a2695a56aa0e5c5bffe85f6aa4bbc312bd
Lin Onetwo <linonetwo012@gmail.com>
2022-12-24 18:45:43 +0800

    chore: upgrade scripts

--- scripts/build-wiki.js
+++ scripts/build-wiki.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+import { fs } from 'zx';
+import path from 'path';
 
 const repoFolder = path.join(path.dirname(__filename), '..');
 const folderToServe = path.join(repoFolder, 'public-dist');
@@ -9,45 +8,44 @@ const folderToServe = path.join(repoFolder, 'public-dist');
 process.env.TIDDLYWIKI_PLUGIN_PATH = `${repoFolder}/plugins`;
 process.env.TIDDLYWIKI_THEME_PATH = `${repoFolder}/themes`;
 
-const execAndLog = (command, options) => console.log(String(execSync(command, options)));
-
-module.exports = function build() {
-  // npm run build:prepare
-  execAndLog(`rm -rf ${folderToServe}`);
+// npm run build:prepare
+await $`rm -rf ${folderToServe}`;
+// npm run build:public
+await $`cp -r ${repoFolder}/public/ ${folderToServe}`;
+try {
+  await $`cp ${repoFolder}/vercel.json ${folderToServe}/vercel.json`;
+} catch (error) {
+  console.log(error);
+}
+// try copy some static assets, don't cause error if some of them been removed by the user
+try {
   // npm run build:public
-  execAndLog(`cp -r ${repoFolder}/public/ ${folderToServe}`, { cwd: repoFolder });
-  execAndLog(`cp ${repoFolder}/vercel.json ${folderToServe}/vercel.json`, { cwd: repoFolder });
-  // try copy some static assets, don't cause error if some of them been removed by the user
-  try {
-    // npm run build:public
-    execAndLog(`cp ${repoFolder}/tiddlers/favicon.ico ${folderToServe}/favicon.ico`, { cwd: repoFolder });
-    execAndLog(`cp ${repoFolder}/tiddlers/TiddlyWikiIconWhite.png ${folderToServe}/TiddlyWikiIconWhite.png`, {
-      cwd: repoFolder,
-    });
-    execAndLog(`cp ${repoFolder}/tiddlers/TiddlyWikiIconBlack.png ${folderToServe}/TiddlyWikiIconBlack.png`, {
-      cwd: repoFolder,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  // npm run build:nodejs2html
-  // exclude edit related plugins, make it readonly, and reduce size
-  execAndLog(`tiddlywiki ${repoFolder} --build readonlyexternalimages`, { cwd: repoFolder });
-  execAndLog(`tiddlywiki ${repoFolder} --build externaljs`, { cwd: repoFolder });
-  // npm run build:sitemap
-  execAndLog(`tiddlywiki . --rendertiddler sitemap sitemap.xml text/plain && mv ${repoFolder}/output/sitemap.xml ${folderToServe}/sitemap.xml`, {
-    cwd: repoFolder,
-  });
-  // npm run build:minifyHTML
-  const htmlMinifyPath = `${repoFolder}/output/index-minify.html`;
-  const htmlOutputPath = `${folderToServe}/index.html`;
-  execAndLog(`html-minifier-terser -c ./html-minifier-terser.config.json -o ${htmlMinifyPath} ${repoFolder}/output/index.html`, { cwd: repoFolder });
-  // build dll.js and config tw to load it
-  // original filename contains invalid char, will cause static server unable to load it
-  const htmlContent = fs.readFileSync(htmlMinifyPath, 'utf-8');
-  const htmlContentWithCorrectJsPath = htmlContent.replaceAll('%24%3A%2Fcore%2Ftemplates%2Ftiddlywiki5.js', 'tiddlywiki5.js');
-  fs.writeFileSync(htmlOutputPath, htmlContentWithCorrectJsPath);
-  execAndLog(`mv ${repoFolder}/output/tiddlywiki5.js ${folderToServe}/tiddlywiki5.js`, { cwd: repoFolder });
-  // npm run build:precache
-  execAndLog(`workbox injectManifest workbox-config.js`, { cwd: repoFolder });
-};
+  await $`cp ${repoFolder}/tiddlers/favicon.ico ${folderToServe}/favicon.ico`;
+  await $`cp ${repoFolder}/tiddlers/TiddlyWikiIconWhite.png ${folderToServe}/TiddlyWikiIconWhite.png`;
+  await $`cp ${repoFolder}/tiddlers/TiddlyWikiIconBlack.png ${folderToServe}/TiddlyWikiIconBlack.png`;
+} catch (error) {
+  console.log(error);
+}
+// npm run build:nodejs2html
+// exclude edit related plugins, make it readonly, and reduce size
+await $`tiddlywiki ${repoFolder} --build readonlyexternalimages`;
+await $`tiddlywiki ${repoFolder} --build externaljs`;
+// npm run build:sitemap
+await $`tiddlywiki . --rendertiddler sitemap sitemap.xml text/plain && mv ${repoFolder}/output/sitemap.xml ${folderToServe}/sitemap.xml`;
+// npm run build:minifyHTML
+const htmlMinifyPath = `${repoFolder}/output/index-minify.html`;
+const htmlOutputPath = `${folderToServe}/index.html`;
+await $`html-minifier-terser -c ./html-minifier-terser.config.json -o ${htmlMinifyPath} ${repoFolder}/output/index.html`;
+// build dll.js and config tw to load it
+// original filename contains invalid char, will cause static server unable to load it
+const htmlContent = fs.readFileSync(htmlMinifyPath, 'utf-8');
+const htmlContentWithCorrectJsPath = htmlContent.replaceAll('%24%3A%2Fcore%2Ftemplates%2Ftiddlywiki5.js', 'tiddlywiki5.js');
+fs.writeFileSync(htmlOutputPath, htmlContentWithCorrectJsPath);
+await $`mv ${repoFolder}/output/tiddlywiki5.js ${folderToServe}/tiddlywiki5.js`;
+// npm run build:precache
+await $`workbox injectManifest workbox-config.js`;
+
+// build downloadable html
+await $`tiddlywiki ${repoFolder} --build externalimages`;
+await $`html-minifier-terser -c ./html-minifier-terser.config.json -o ${htmlMinifyPath} ${repoFolder}/output/index.html`;
+await $`mv ${htmlMinifyPath} ${folderToServe}/index-full.html`;
