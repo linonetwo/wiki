e59dfe59593d3d5b19884bedb02572abe9ac9562
linonetwo <linonetwo012@gmail.com>
2023-02-03 10:37:09 +0800

    Updated with Git-Sync

--- tiddlers/startBlogAndProxy.config.js
+++ tiddlers/startBlogAndProxy.config.js
@@ -3,12 +3,13 @@ module.exports = {
   {
     name: "tw",
     script: "tiddlywiki ~/wiki --listen host=0.0.0.0 port=8080 root-tiddler=$:/core/save/lazy-images tls-key=../ssl/wiki.onetwo.ren.key tls-cert=../ssl/wiki.onetwo.ren_public.crt",
-		watch: ['~/wiki'],
+    watch: ["~/wiki"],
+		watch_delay: 1000,
   },
   {
     name: "git",
     script: "cd ~/wiki && git pull",
-		cron_restart: '0 0,12 * * *',
+		cron_restart: '*/10 * * * *',
   },
   {
      name: 'proxy',
