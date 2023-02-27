5506afdf58a7bf4c410d962f0c2858d6b9112dc7
linonetwo <linonetwo012@gmail.com>
2023-02-28 00:54:44 +0800

    Updated with Git-Sync

--- tiddlers/startBlogAndProxy.config.js
+++ tiddlers/startBlogAndProxy.config.js
@@ -3,7 +3,11 @@ const path = require('path');
 const userHomeDir = os.homedir();
 const wikiDir = path.resolve(userHomeDir, 'wiki');
 
-// crontab -e */60 * * * * pm2 restart git
+// pm2 cron and watch are not working at all, so use crontab instead
+// crontab -e
+// to open the crontab config file, and add following line
+// */60 * * * * https_proxy="192.168.x.x:xxxx" pm2 restart git
+// */60 * * * * pm2 restart tw
 // That will restart git pull every hour! https://github.com/Unitech/pm2/issues/1076#issuecomment-441085488
 
 module.exports = {
