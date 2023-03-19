4fcfcea5fd6b9a0da6021a13058febb185ac2fd8
linonetwo <linonetwo012@gmail.com>
2023-03-19 17:58:31 +0800

    .

--- tiddlers/将模型库的内容软链接到SDWebUI里.mjs.js
+++ tiddlers/将模型库的内容软链接到SDWebUI里.mjs.js
@@ -1,52 +1,57 @@
-const home = os.homedir();
+let modelHome = '';
+
+echo(os.type().toLowerCase())
+switch (os.type().toLowerCase()) {
+  case 'darwin': {
+    modelHome = `/Volumes/model`;
+		break;
+  }
+  case 'windows_nt': {
+    modelHome = `E:/model`;
+		break;
+  }
+}
+
+if (!modelHome) throw new Error(`modelHome not defined, os.type(): ${os.type()}`);
+const sduiHome = `${os.homedir()}/Desktop/repo/stable-diffusion-webui`;
 
 const models = [
-  `${home}/Desktop/model/Model/AbyssOrangeMix2-Hardcore/abyssorangemix2_Hard.safetensors`,
-  `${home}/Desktop/model/Model/Anything/anything-v4.5-pruned.safetensors`,
-  `${home}/Desktop/model/Model/chilloutmix/chilloutmix_NiPrunedFp32Fix.safetensors`,
-  `${home}/Desktop/model/Model/novelai/animefull-final-pruned/novelai-animefull-final-pruned.ckpt`,
-  `${home}/Desktop/model/Model/perfectWorld/perfectWorld_v2Baked.safetensors`,
-  `${home}/Desktop/model/Model/PVC/aoaokoPVCStyleModel_pvcAOAOKO.safetensors`,
-  `${home}/Desktop/model/Model/StableDiffussion/StableDiffussion-v1-5-pruned-emaonly.ckpt`,
+  `${modelHome}/Model/AbyssOrangeMix2-Hardcore/abyssorangemix2_Hard.safetensors`,
+  `${modelHome}/Model/Anything/anything-v4.5-pruned.safetensors`,
+  `${modelHome}/Model/chilloutmix/chilloutmix_NiPrunedFp32Fix.safetensors`,
+  `${modelHome}/Model/novelai/animefull-final-pruned/novelai-animefull-final-pruned.ckpt`,
+  `${modelHome}/Model/perfectWorld/perfectWorld_v2Baked.safetensors`,
+  `${modelHome}/Model/PVC/aoaokoPVCStyleModel_pvcAOAOKO.safetensors`,
+  `${modelHome}/Model/StableDiffussion/StableDiffussion-v1-5-pruned-emaonly.ckpt`,
 ];
 
-await Promise.all(
-  models.map((location) => $`ln -sf ${location} ${home}/Desktop/repo/stable-diffusion-webui/models/Stable-diffusion`)
-).catch(() => {});
+await Promise.all(models.map((location) => $`ln -sf ${location} ${sduiHome}/models/Stable-diffusion`)).catch(() => {});
 
 const vaes = [
-  `${home}/Desktop/model/Model/novelai/novelai-animefull-final-pruned-vae.pt`,
-  `${home}/Desktop/model/Model/AbyssOrangeMix2-Hardcore/orangemix.vae.pt`,
+  `${modelHome}/Model/novelai/novelai-animefull-final-pruned-vae.pt`,
+  `${modelHome}/Model/AbyssOrangeMix2-Hardcore/orangemix.vae.pt`,
 ];
 
-await Promise.all(
-  vaes.map((location) => $`ln -sf ${location} ${home}/Desktop/repo/stable-diffusion-webui/models/VAE`)
-).catch(() => {});
+await Promise.all(vaes.map((location) => $`ln -sf ${location} ${sduiHome}/models/VAE`)).catch(() => {});
 
-const controlNets = [`${home}/Desktop/model/extensions/ControlNet/control_sd15_openpose.pth`];
+const controlNets = [`${modelHome}/extensions/ControlNet/control_sd15_openpose.pth`];
 
 await Promise.all(
-  controlNets.map(
-    (location) => $`ln -sf ${location} ${home}/Desktop/repo/stable-diffusion-webui/extensions/sd-webui-controlnet/models`
-  )
+  controlNets.map((location) => $`ln -sf ${location} ${sduiHome}/extensions/sd-webui-controlnet/models`)
 ).catch(() => {});
 
-const LoRAFolder = `${home}/Desktop/model/LoRA`;
+const LoRAFolder = `${modelHome}/LoRA`;
 const LoRAs = (await fs.readdir(LoRAFolder))
   .filter((location) => !location.startsWith('.'))
   .map((item) => `${LoRAFolder}/${item}`);
 
-await Promise.all(
-  LoRAs.map((location) => $`ln -sf ${location} ${home}/Desktop/repo/stable-diffusion-webui/models/Lora`)
-).catch(() => {});
+await Promise.all(LoRAs.map((location) => $`ln -sf ${location} ${sduiHome}/models/Lora`)).catch(() => {});
 
-const TextualInversionEmbeddingsFolder = `${home}/Desktop/model/TextualInversionEmbeddings`;
+const TextualInversionEmbeddingsFolder = `${modelHome}/TextualInversionEmbeddings`;
 const TextualInversionEmbeddings = (await fs.readdir(TextualInversionEmbeddingsFolder))
   .filter((location) => !location.startsWith('.'))
   .map((item) => `${TextualInversionEmbeddingsFolder}/${item}`);
 
-await Promise.all(
-  TextualInversionEmbeddings.map(
-    (location) => $`ln -sf ${location} ${home}/Desktop/repo/stable-diffusion-webui/embeddings`
-  )
-).catch(() => {});
+await Promise.all(TextualInversionEmbeddings.map((location) => $`ln -sf ${location} ${sduiHome}/embeddings`)).catch(
+  () => {}
+);
